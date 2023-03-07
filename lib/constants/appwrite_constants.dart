7ce64dc204eeaf74d289750bef00cb2ece66c6f0
thejitenpatel <jiten.pateljp2001@gmail.com>
2023-03-07 12:45:31 +0530

    first commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstant {
+  static const String databaesId = "6406e2806ac155ed8616";
+  static const String projectId = "6405f6c496fc8f9667b7";
+  static const String endPoint = "http://localhost:80/v1";
+}
