000b8f3cfa88e2f260e87d513db525913db314f9
thejitenpatel <jiten.pateljp2001@gmail.com>
2023-03-07 15:04:47 +0530

    login view completed

---
+++ lib/common/rounded_small_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:twitter_clone/theme/pallete.dart';
+
+class RoundedSmallButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String label;
+  final Color backgroundColor;
+  final Color textColor;
+
+  const RoundedSmallButton({
+    super.key,
+    required this.onTap,
+    required this.label,
+    this.backgroundColor = Pallete.whiteColor,
+    this.textColor = Pallete.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Chip(
+      label: Text(
+        label,
+        style: TextStyle(color: textColor),
+      ),
+      backgroundColor: backgroundColor,
+      labelPadding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 5,
+      ),
+    );
+  }
+}
---
+++ lib/features/auth/view/login_view.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:twitter_clone/common/rounded_small_button.dart';
+import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
+import 'package:twitter_clone/theme/pallete.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final appBar = UIConstants.appBar();
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child: Column(
+              children: [
+                AuthField(
+                  controller: emailController,
+                  hintText: "Email",
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                AuthField(
+                  controller: passwordController,
+                  hintText: "Password",
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: RoundedSmallButton(
+                    onTap: () {},
+                    label: "Done",
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: "Don't have account?",
+                    style: const TextStyle(
+                      fontSize: 16,
+                    ),
+                    children: const [
+                      TextSpan(
+                        text: " Sign Up",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Pallete.blueColor,
+                        ),
+                      )
+                    ],
+                    recognizer: TapGestureRecognizer()..onTap = () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
