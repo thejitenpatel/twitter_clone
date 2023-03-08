5e14dffeaed7a7f43e867695e9cc75cf8589155f
thejitenpatel <jiten.pateljp2001@gmail.com>
2023-03-08 21:03:25 +0530

    auth api created

---
+++ lib/core/type_defs.dart
@@ -0,0 +1,9 @@
+import 'dart:ffi';
+
+import 'package:fpdart/fpdart.dart';
+import 'package:twitter_clone/core/core.dart';
+
+typedef FutureEither<T> = Future<Either<Failure, T>>;
+typedef FutureEitherVoid = FutureEither<void>;
+
+
